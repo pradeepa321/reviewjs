f4896e2414168bf86a6dafcdf4d6a1a785d076fc
pradeepamurugan <kmrpradeepa123@gmail.com>
2023-03-29 15:30:26 +0530

    update

---
+++ main.js
@@ -0,0 +1,48 @@
+			
+	var a1=345;
+	var b1=0;
+	while(a1>0){
+		c=a1%10;
+		a1=(a1-c)/10;
+		if(c==1){
+			
+			document.write("one");
+		}
+		else if(c==2){
+			
+			document.write("two");
+		}
+		else if(c==3){
+			
+			document.write("three");
+		}
+		else if(c==4){
+			
+			document.write("four");
+		}
+		else if(c==5){
+			
+			document.write("five");
+		}
+		else if(c==6){
+			
+			document.write("six");
+		}
+		else if(c==7){
+			
+			document.write("seven");
+		}
+		else if(c==8){
+			
+			document.write("eight");
+		}
+		else if(c==9){
+			
+			document.write("nine");
+		}
+		else if(c==0){
+			
+			document.write("zero");
+		}
+		
+	}
